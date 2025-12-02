8a84cbf35e175d2c07792e55c8ed39a19d85b412
Nantipart <jamelopesnan@gmail.com>
2025-12-02 17:51:58 +0700

    5

--- engce117/1-2.c
+++ engce117/1-2.c
@@ -4,29 +4,28 @@
 void GetSet(int **data, int *num);
 
 int main() {
-    int *data;
-    int num;
+    int *data, num;
 
-    GetSet(&data, &num);
+    GetSet(&data, &num); // Get data and print members
 
-    printf("\nMembers of the received set :\n");
-    for(int i = 0; i < num; i++){
-        printf("%d ", data[i]);
-    }
-
-    free(data);
     return 0;
 }
 
 void GetSet(int **data, int *num) {
-    printf("Number of members : ");
-    scanf("%d", num);
+    printf("Number of members: ");
+    scanf("%d", num);  // Must be fixed to &(*num)
 
-    *data = malloc(*num * sizeof(int));
+    *data = malloc(*num * sizeof(int)); // Allocate memory for the array
 
-    printf("Enter values ​​one member at a time :\n");
+    printf("Enter values one by one:\n");
     for(int i = 0; i < *num; i++){
         printf("Member %d: ", i+1);
-        scanf("%d", &(*data)[i]);
+        scanf("%d", &(*data)[i]); // Get value of each member
     }
+
+    printf("\nMembers in the set received:\n");
+    for(int i = 0; i < *num; i++){
+        printf("%d ", (*data)[i]); // Print all members
+    }
+    printf("\n");
 }
