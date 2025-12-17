dbafdc33d32bb514e0efa9012bc91cc95c245d41
Nantipart <jamelopesnan@gmail.com>
2025-12-17 23:34:53 +0700

    Update 4-1.c

--- basic/L-4/4-1.c
+++ basic/L-4/4-1.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
+// Function to read numbers and print in reverse
+void readReverse(int n) {
+    int x;
+    if (n == 0)
+        return;
+
+    scanf("%d", &x);
+    readReverse(n - 1);
+    printf("%d\n", x);
+}
+
 int main() {
-    int N, i;
+    int N;
 
-    // 1. Receive number of elements (N)
-    if (scanf("%d", &N) != 1) {
-        return 1; // Exit if input is invalid
-    }
+    if (scanf("%d", &N) != 1)
+        return 1;
 
-    int scores[N];
-
-    for (i = 0; i < N; i++) {
-        if (scanf("%d", &scores[i]) != 1) {
-            return 1;
-        }
-    }
-
-    for (i = N - 1; i >= 0; i--) {
-        printf("%d\n", scores[i]);
-    }
+    readReverse(N);
 
     return 0;
 }
