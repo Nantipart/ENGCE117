16595b6c335e3d26437e8a17c3973a49f297f06c
Nantipart <jamelopesnan@gmail.com>
2025-12-17 20:20:16 +0700

    Update 2-3.c

--- basic/L-2/2-3.c
+++ basic/L-2/2-3.c
@@ -2,47 +2,24 @@
 
 int main() {
     int termCode;
-    float investmentAmount;
-    float aprRate = 0.0;
-    float totalInterest = 0.0;
+    float investmentAmount, rate, interest;
 
-    if (scanf("%d %f", &termCode, &investmentAmount) != 2) {
-        return 1; // Exit if input is invalid
-    }
+    if (scanf("%d %f", &termCode, &investmentAmount) != 2)
+        return 1;
 
-    switch (termCode) {
-        case 1: 
-            // Check investment amount condition for Term 1
-            if (investmentAmount < 5000) {
-                aprRate = 0.03; // 3.0% APR
-            } else {
-                aprRate = 0.04; // 4.0% APR
-            }
-            break;
-
-        case 2:
-            // Check investment amount condition for Term 2
-            if (investmentAmount < 10000) {
-                aprRate = 0.05; // 5.0% APR
-            } else {
-                aprRate = 0.065; // 6.5% APR
-            }
-            break;
-
-        case 3: 
-            // Fixed rate for Term 3
-            aprRate = 0.08; // 8.0% APR
-            break;
-
-        default:
-            break;
-    }
-    if (termCode >= 1 && termCode <= 3) {
-        totalInterest = investmentAmount * aprRate;
-        printf("%.2f\n", totalInterest);
-    } else {
+    if (termCode == 1)
+        rate = (investmentAmount < 5000) ? 0.03 : 0.04;
+    else if (termCode == 2)
+        rate = (investmentAmount < 10000) ? 0.05 : 0.065;
+    else if (termCode == 3)
+        rate = 0.08;
+    else {
         printf("Invalid Term Code\n");
+        return 0;
     }
+
+    interest = investmentAmount * rate;
+    printf("%.2f\n", interest);
 
     return 0;
 }
