e688ab4c815143a3dc81e2d9b41e5976d037a8d0
nantipart <jamelopesnan@gmail.com>
2025-12-25 11:08:45 +0700

    Update 3-1.c

--- engce117/lab3/3-1.c
+++ engce117/lab3/3-1.c
@@ -1,37 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 struct student {
-    char name[ 20 ];
+    char name[20];
     int age;
     char sex;
     float gpa;
 };
 
-void upgrade( struct student child );
+void upgrade(struct student *child);
 
 int main() {
     struct student aboy;
 
-    // Initialize data.
     aboy.sex = 'M';
     aboy.gpa = 3.00;
 
-    upgrade( aboy );
+    upgrade(&aboy);
 
-    // Print result (GPA will remain 3.00 because the original was not modified).
-    printf( "%.2f", aboy.gpa );
-    
+    printf("%.2f", aboy.gpa);  // Output: 3.30
     return 0;
 }
 
-void upgrade( struct student child ) {
-    // Check sex and apply multiplier locally.
-    if ( child.sex == 'M' ) {
-        child.gpa *= 1.10; 
-        // Increase 10% for Male.
+void upgrade(struct student *child) {
+    if (child->sex == 'M') {
+        child->gpa *= 1.10;
     } else {
-        child.gpa *= 1.20; 
-        // Increase 20% for Female.
+        child->gpa *= 1.20;
     }
 }
