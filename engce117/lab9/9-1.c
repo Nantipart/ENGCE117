b6271593bd8d601725bcdba3da00e22f576d3e7d
Nantipart <jamelopesnan@gmail.com>
2026-02-19 20:54:16 +0700

    2

--- engce117/lab9/9-1.c
+++ engce117/lab9/9-1.c
@@ -1,3 +1,143 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+struct student {
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+};
+
+void addData() {
+    FILE *fp = fopen("student.dat", "ab");
+    struct student s;
+
+    printf("Name: "); scanf("%s", s.name);
+    printf("Age: "); scanf("%d", &s.age);
+    printf("Sex: "); scanf(" %c", &s.sex);
+    printf("GPA: "); scanf("%f", &s.gpa);
+
+    fwrite(&s, sizeof(struct student), 1, fp);
+    fclose(fp);
+}
+
+void showAll() {
+    FILE *fp = fopen("student.dat", "rb");
+    struct student s;
+
+    if (fp == NULL) return;
+
+    while (fread(&s, sizeof(struct student), 1, fp)) {
+        printf("%s %d %c %.2f\n",
+               s.name, s.age, s.sex, s.gpa);
+    }
+    fclose(fp);
+}
+
+void findData() {
+    FILE *fp = fopen("student.dat", "rb");
+    struct student s;
+    char search[20];
+    int found = 0;
+
+    printf("Search name: ");
+    scanf("%s", search);
+
+    while (fread(&s, sizeof(struct student), 1, fp)) {
+        if (strcmp(s.name, search) == 0) {
+            printf("Found: %s %d %c %.2f\n",
+                   s.name, s.age, s.sex, s.gpa);
+            found = 1;
+            break;
+        }
+    }
+    if (!found)
+        printf("Not found!\n");
+
+    fclose(fp);
+}
+
+void deleteData() {
+    FILE *fp = fopen("student.dat", "rb");
+    FILE *temp = fopen("temp.dat", "wb");
+    struct student s;
+    char delname[20];
+    int found = 0;
+
+    printf("Delete name: ");
+    scanf("%s", delname);
+
+    while (fread(&s, sizeof(struct student), 1, fp)) {
+        if (strcmp(s.name, delname) != 0)
+            fwrite(&s, sizeof(struct student), 1, temp);
+        else
+            found = 1;
+    }
+
+    fclose(fp);
+    fclose(temp);
+
+    remove("student.dat");
+    rename("temp.dat", "student.dat");
+
+    if (found)
+        printf("Deleted successfully\n");
+    else
+        printf("Not found\n");
+}
+
+void editData() {
+    FILE *fp = fopen("student.dat", "rb+");
+    struct student s;
+    char editname[20];
+    int found = 0;
+
+    printf("Edit name: ");
+    scanf("%s", editname);
+
+    while (fread(&s, sizeof(struct student), 1, fp)) {
+        if (strcmp(s.name, editname) == 0) {
+            printf("New age: "); scanf("%d", &s.age);
+            printf("New sex: "); scanf(" %c", &s.sex);
+            printf("New GPA: "); scanf("%f", &s.gpa);
+
+            fseek(fp, -sizeof(struct student), SEEK_CUR);
+            fwrite(&s, sizeof(struct student), 1, fp);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+        printf("Not found!\n");
+
+    fclose(fp);
+}
+
+int main() {
+    int menu;
+
+    do {
+        printf("\nMenu\n");
+        printf("1. Add\n");
+        printf("2. Edit\n");
+        printf("3. Delete\n");
+        printf("4. Find\n");
+        printf("5. Show All\n");
+        printf("0. Exit\n");
+        printf("Select: ");
+        scanf("%d", &menu);
+
+        switch(menu) {
+            case 1: addData(); break;
+            case 2: editData(); break;
+            case 3: deleteData(); break;
+            case 4: findData(); break;
+            case 5: showAll(); break;
+        }
+
+    } while(menu != 0);
+
+    return 0;
+}
