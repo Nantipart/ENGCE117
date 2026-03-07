e862f471d43efb5519c0f2217f7631b13da50c3a
Nantipart <jamelopesnan@gmail.com>
2026-03-07 19:12:25 +0700

    Update 13-1.c

--- engce117/lab13/13-1.c
+++ engce117/lab13/13-1.c
@@ -5,56 +5,47 @@ int *KnapsackDP(int *w, int *v, int n, int wx)
 {
     int i, j;
 
-    int **dp = (int**)malloc((n+1) * sizeof(int*));
+    int **table;
+    table = (int**)malloc((n+1)*sizeof(int*));
+
     for(i = 0; i <= n; i++)
-        dp[i] = (int*)calloc(wx+1, sizeof(int));
+        table[i] = (int*)malloc((wx+1)*sizeof(int));
+
+    for(i = 0; i <= n; i++)
+        for(j = 0; j <= wx; j++)
+            table[i][j] = 0;
 
     for(i = 1; i <= n; i++)
     {
         for(j = 0; j <= wx; j++)
         {
+            table[i][j] = table[i-1][j];
+
             if(w[i-1] <= j)
             {
-                int a = v[i-1] + dp[i-1][j-w[i-1]];
-                int b = dp[i-1][j];
+                int candidate = table[i-1][j-w[i-1]] + v[i-1];
 
-                if(a > b)
-                    dp[i][j] = a;
-                else
-                    dp[i][j] = b;
+                if(candidate > table[i][j])
+                    table[i][j] = candidate;
             }
-            else
-                dp[i][j] = dp[i-1][j];
         }
     }
 
-    int *x = (int*)calloc(n, sizeof(int));
+    int *choose = (int*)calloc(n,sizeof(int));
 
+    i = n;
     j = wx;
-    for(i = n; i > 0; i--)
+
+    while(i > 0 && j >= 0)
     {
-        if(dp[i][j] != dp[i-1][j])
+        if(table[i][j] != table[i-1][j])
         {
-            x[i-1] = 1;
+            choose[i-1] = 1;
             j = j - w[i-1];
         }
+
+        i--;
     }
 
-    return x;
-}
-
-int main()
-{
-    int n = 5, wx = 11;
-    int w[5] = {1,2,5,6,7};
-    int v[5] = {1,6,18,22,28};
-
-    int *x;
-
-    x = KnapsackDP(w,v,n,wx);
-
-    for(int i=0;i<n;i++)
-        printf("%d ",x[i]);
-
-    return 0;
+    return choose;
 }
