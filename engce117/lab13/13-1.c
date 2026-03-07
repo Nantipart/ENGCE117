b9cf2b0520ee068c716c82f0efeb013c1fbc6911
Nantipart <jamelopesnan@gmail.com>
2026-03-07 18:46:39 +0700

    Update 13-1.c

--- engce117/lab13/13-1.c
+++ engce117/lab13/13-1.c
@@ -1,26 +1,20 @@
-#include <stdio.h>
 #include <stdlib.h>
 
-// Dynamic Programming Knapsack function
 int *KnapsackDP(int *w, int *v, int n, int wx)
 {
     int i, j;
 
-    // DP table
-    int dp[n+1][wx+1];
-
+    int **dp = (int**)malloc((n+1) * sizeof(int*));
     for(i = 0; i <= n; i++)
-        for(j = 0; j <= wx; j++)
-            dp[i][j] = 0;
+        dp[i] = (int*)calloc(wx+1, sizeof(int));
 
-    // Build DP table
     for(i = 1; i <= n; i++)
     {
         for(j = 0; j <= wx; j++)
         {
             if(w[i-1] <= j)
             {
-                int take = v[i-1] + dp[i-1][j-w[i-1]];
+                int take = v[i-1] + dp[i-1][j - w[i-1]];
                 int nottake = dp[i-1][j];
 
                 if(take > nottake)
@@ -35,12 +29,10 @@ int *KnapsackDP(int *w, int *v, int n, int wx)
         }
     }
 
-    // result array
     int *x = (int*)calloc(n, sizeof(int));
 
     j = wx;
 
-    // trace back solution
     for(i = n; i > 0; i--)
     {
         if(dp[i][j] != dp[i-1][j])
@@ -51,21 +43,4 @@ int *KnapsackDP(int *w, int *v, int n, int wx)
     }
 
     return x;
-}
-
-int main()
-{
-    int n = 5, wx = 11;
-
-    int w[5] = {1, 2, 5, 6, 7};
-    int v[5] = {1, 6, 18, 22, 28};
-
-    int *x;
-
-    x = KnapsackDP(w, v, n, wx);
-
-    for(int i = 0; i < n; i++)
-        printf("%d ", x[i]);
-
-    return 0;
 }
